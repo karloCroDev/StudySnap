2e15d73a624b5fc9b69f1ccf7614d17fe81fc5df
karloCroDev <karlo.webdev@gmail.com>
2024-11-23 20:38:44 +0100

    Created drawer component, needs to compose the header component

--- src/app/(core)/layout.tsx
+++ src/app/(core)/layout.tsx
@@ -1,6 +1,6 @@
 'use client';
 // External packages
-import { HamburgerMenuIcon } from '@radix-ui/react-icons';
+import { GearIcon, HamburgerMenuIcon, PersonIcon } from '@radix-ui/react-icons';
 
 // Components
 import { Layout } from '@/components/ui/Layout';
@@ -43,13 +43,15 @@ export default function CoreLayout({
 // External packages
 import * as React from 'react';
 import * as RadixDialog from '@radix-ui/react-dialog';
-import { MagnifyingGlassIcon } from '@radix-ui/react-icons';
+import { HomeIcon, FrameIcon, ExitIcon } from '@radix-ui/react-icons';
+import Image from 'next/image';
 
 // Components
 import { LinkAsButton } from '@/components/ui/LinkAsButton';
+import { Button } from '@/components/ui/Button';
 
-// // Images
-// import
+// Images
+import example from '@/public/images/login-image.png';
 
 const Drawer = () => {
   console.log('Hello world');
@@ -59,7 +61,76 @@ const Drawer = () => {
         <HamburgerMenuIcon className="size-7" />
       </RadixDialog.Trigger>
       <RadixDialog.Overlay className="fixed left-0 top-0 h-screen w-screen bg-gray-900 opacity-60 data-[state=closed]:animate-overlay-closed data-[state=open]:animate-overlay-open" />
-      <RadixDialog.Content className="data-[state=open]:animate-drawer-slide-open data-[state=closed]:animate-drawer-slide-closed text-grayscale-10 fixed left-0 top-0 z-max flex h-full w-3/4 flex-col overflow-scroll overflow-x-hidden rounded-r-xl bg-gray-100 px-3 py-6"></RadixDialog.Content>
+      <RadixDialog.Content className="data-[state=open]:animate-drawer-slide-open data-[state=closed]:animate-drawer-slide-closed text-grayscale-10 fixed left-0 top-0 z-max flex h-full w-3/4 flex-col overflow-scroll overflow-x-hidden rounded-r-xl bg-gray-100 px-3 py-6 outline-none">
+        <div className="flex flex-col items-center">
+          <Image
+            src={example}
+            alt="Hello world"
+            className="size-36 rounded-full object-cover"
+          />
+          <h4 className="mt-6 text-lg font-semibold">Ana horvat</h4>
+        </div>
+        <hr className="mt-3 h-px w-full border-0 bg-gray-900" />
+        <ul className="mt-6 flex flex-col gap-3">
+          <li>
+            <LinkAsButton
+              href="/home"
+              colorScheme="dark-blue"
+              size="lg"
+              iconLeft={<HomeIcon className="size-6" />}
+              className="justify-start"
+            >
+              Home
+            </LinkAsButton>
+          </li>
+          <li>
+            <LinkAsButton
+              href="/discover"
+              colorScheme="white"
+              size="lg"
+              iconLeft={<FrameIcon className="size-6" />}
+              className="justify-start"
+            >
+              Discover
+            </LinkAsButton>
+          </li>
+        </ul>
+        <hr className="mt-auto h-px w-full border-0 bg-gray-900" />
+
+        <ul className="mt-6 flex flex-col gap-4">
+          <li>
+            {/* Dialog treba ubaciti */}
+            <LinkAsButton
+              href="/discover"
+              colorScheme="white"
+              iconLeft={<PersonIcon className="size-6" />}
+              className="justify-start"
+            >
+              Edit profile
+            </LinkAsButton>
+          </li>
+          <li>
+            <LinkAsButton
+              href="/discover"
+              colorScheme="white"
+              iconLeft={<GearIcon className="size-6" />}
+              className="justify-start"
+            >
+              Public settings
+            </LinkAsButton>
+          </li>
+          <li>
+            <Button
+              colorScheme="white"
+              size="lg"
+              iconLeft={<ExitIcon className="size-6" />}
+              className="w-full justify-start bg-red-400 text-gray-100"
+            >
+              Log out
+            </Button>
+          </li>
+        </ul>
+      </RadixDialog.Content>
     </RadixDialog.Root>
   );
 };
