3f69f6321f02e03647855f19a012b87198885f3a
karloCroDev <karlo.webdev@gmail.com>
2024-12-21 16:05:04 +0100

    Fixed the height problem!

--- src/app/(core)/note-editor/page.tsx
+++ src/app/(core)/note-editor/page.tsx
@@ -11,8 +11,8 @@ import { plus_jakarta_sans } from '@/libs/fonts';
 
 export default function Note() {
   return (
-    <LayoutRow className="h-[calc(100vh-132px)] justify-center pb-8 lg:h-[calc(100vh-168px)]">
-      <LayoutColumn lg={10} className="flex flex-col overflow-y-scroll">
+    <LayoutRow className="h-[calc(100vh-132px-32px)] justify-center overflow-hidden lg:h-[calc(100vh-168px-32px)]">
+      <LayoutColumn lg={10} className="flex h-full flex-col">
         <div className="flex items-center justify-between">
           <h1
             className={twJoin(
@@ -22,7 +22,7 @@ export default function Note() {
           >
             WWII
           </h1>
-          <p className="text-md font-semibold italic text-gray-500">by: You</p>
+          <p className="text-md font-semibold text-gray-500">by: You</p>
         </div>
         <TipTapEditor />
       </LayoutColumn>
