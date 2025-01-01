bdfc16811338e15d8ad1747c1dad8f6b774138d2
Luka B <luka218393@gmail.com>
2025-01-01 20:53:21 +0100

    Add sign-up

--- src/app/api/database/pool.js
+++ src/app/api/database/pool.js
@@ -3,7 +3,7 @@ import { databaseConnectionObject } from "../../../../Secrets.js"
 
 export const pool = createPool(databaseConnectionObject).promise()
 
-export async function QueryUserByEmail(email) {
+export async function GetUserByEmail(email) {
     const result = await pool.query(`SELECT * FROM user WHERE email = "${email}"`);
     return result[0][0];
 }
@@ -12,5 +12,3 @@ export async function IsUsernameOrEmailTaken(username, email) {
     const result = await pool.query(`SELECT * FROM user WHERE username = "${username}" OR email = "${email}"`);
     return result[0].length > 0;
 }
-//console.log(await IsUsernameOrEmailTaken("gyf", "lkshgvf")) // Should output true if connected to database
-//pool.end()
