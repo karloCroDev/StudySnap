a57056d481ecbbafc8d0163e03b93f3d8ff34147
Luka B. <luka218393@gmail.com>
2025-01-01 20:53:21 +0100

    Add sign-up

--- src/app/api/auth/sign-up/route.ts
+++ src/app/api/auth/sign-up/route.ts
@@ -1,29 +1,25 @@
 // External packages
 import { NextResponse } from 'next/server';
 import bcrypt from 'bcryptjs';
-
-// libs
-import { connectMongoDB } from '@/lib/db';
-
+import { IsUsernameOrEmailTaken } from '../../database/pool';
 // Models
-import { User } from '@/models/user';
+import { User } from '@/models/models';
 
 export async function POST(req: Request) {
   try {
-    const { username, email, password } = await req.json();
+    const { username, full_name, email, password } = await req.json();
+    console.log(username, full_name, email, password);
+
     const hashedPassword = await bcrypt.hash(password, 10);
     // 10 is the number of rounds to generate the salt
-    await connectMongoDB();
-    const existingUser = await User.findOne({ email });
-    if (existingUser) {
-      return NextResponse.json('Email already in use.', { status: 400 });
+    if (!username || !email || !password) {
+      return NextResponse.json('Insufficient data provided', { status: 200 });
     }
-    await User.create({
-      username,
-      email,
-      password: hashedPassword,
-    });
-    return NextResponse.json('User registred', { status: 200 });
+    if (await IsUsernameOrEmailTaken(username)) {
+      return NextResponse.json('Username or email is already taken', { status: 200 });
+    }
+    await new User(username, "full_name", email, hashedPassword).Insert()
+    return NextResponse.json('User registred', { status: 201 });
   } catch (error) {
     console.error(error);
     return NextResponse.json('Failed to sign up', { status: 500 });
