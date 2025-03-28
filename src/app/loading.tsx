e67e6bf18c36fca70bca6076b0f061d8ca444616
karloCroDev <karlo.webdev@gmail.com>
2025-03-28 01:48:39 +0100

    Loading screen is polished

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,12 +1,22 @@
-// Components
-import { Spinner } from '@/components/ui/Spinner';
-import { Logo } from '@/components/ui/Logo';
+// External packages
+import Image from 'next/image';
 
-export default function Loading() {
+export default function NotFound() {
   return (
-    <div className="flex h-[100vh] w-full items-center justify-center gap-8">
-      <Logo className="animate-bounce" />
-      <Spinner size="md" />
+    <div className="flex h-[100vh] w-full flex-col items-center justify-center gap-8">
+      <div className="relative size-36">
+        <Image
+          src="/images/favicon-logo.png"
+          alt="Logo of StudySnap"
+          className="h-full w-full object-cover"
+          fill
+        />
+      </div>
+      <ul className="flex gap-4">
+        <li className="size-4 animate-bounce rounded-full bg-blue-400 [animation-delay:-0.3s]"></li>
+        <li className="size-4 animate-bounce rounded-full bg-blue-400 [animation-delay:-0.15s]"></li>
+        <li className="size-4 animate-bounce rounded-full bg-blue-400"></li>
+      </ul>
     </div>
   );
 }
