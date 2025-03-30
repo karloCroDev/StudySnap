c3c038bbdb9ed2905e4f39c9b2dbee6ab43d857b
karloCroDev <karlo.webdev@gmail.com>
2025-03-30 03:53:21 +0200

    Added offset tack with localstorage

--- src/app/api/core/discover/route.ts
+++ src/app/api/core/discover/route.ts
@@ -5,41 +5,12 @@ import { NextResponse, NextRequest } from 'next/server';
 import { GetPublicNotes } from '@/db/core/discover';
 import { SQLSyntaxCheck } from '@/lib/algorithms/stringVerification';
 
-// Karlo: Put this insdie the intial request on Discover page
-//Function that gets all of the notes that are avalible to the public
-export async function GET(req: NextRequest) {
-  try {
-    const { userId, filter } = await req.json();
-
-    let notes = await GetPublicNotes(20, 0, userId, filter ?? '');
-
-    if (
-      !notes
-      //  || SQLSyntaxCheck([userId, filter])
-    ) {
-      return NextResponse.json({ message: 'Bad request' }, { status: 400 });
-    }
-
-    return NextResponse.json(notes, { status: 200 });
-  } catch (error) {
-    console.error(error);
-    return NextResponse.json(
-      {
-        message: 'Failed to get notes',
-      },
-      {
-        status: 500,
-      }
-    );
-  }
-}
-
 //Function that gets all of the notes that are avalible to the public
 export async function POST(req: NextRequest) {
   try {
-    const { userId, filter } = await req.json();
+    const { userId, filter, offset = 0, limit = 4 } = await req.json();
 
-    let notes = await GetPublicNotes(10, 0, userId, filter ?? '');
+    let notes = await GetPublicNotes(limit, offset, userId, filter ?? '');
 
     if (
       !notes
--- src/hooks/core/discover/useExploreNotes.ts
+++ src/hooks/core/discover/useExploreNotes.ts
@@ -8,6 +8,7 @@ import { useToastStore } from '@/store/useToastStore';
 
 // Models (types)
 import { type Note } from '@/models/note';
+import { useLocalStorage } from '@/hooks/useLocalStorage';
 
 export const useExploreNotes = ({
   userId,
@@ -16,8 +17,13 @@ export const useExploreNotes = ({
   userId: number;
   setNotes: React.Dispatch<React.SetStateAction<Note[]>>;
 }) => {
+  const { getItem, setItem } = useLocalStorage('offset'); // Making sure that when explore more is clicked, even when refreshed new unseen values will be displayed
   const [loadingExplore, setLoadingExplore] = React.useState(false);
 
+  // Karlo: Put 6 when more notes come
+  const [offset, setOffset] = React.useState(getItem() || 0);
+
+  console.log(offset);
   const toast = useToastStore((state) => state.setToast);
 
   const exploreNotes = async () => {
@@ -28,7 +34,7 @@ export const useExploreNotes = ({
         headers: {
           'Content-Type': 'application/json',
         },
-        body: JSON.stringify({ userId }),
+        body: JSON.stringify({ userId, offset, limit: 1 }),
       });
       if (!response.ok)
         toast({
@@ -38,6 +44,10 @@ export const useExploreNotes = ({
         });
       const data = await response.json();
       setNotes((prev) => [...prev, ...data]);
+      // Karlo: Put 6 when more notes come
+      const updatedOffset = offset + 1;
+      setOffset(updatedOffset);
+      setItem(updatedOffset);
     } catch (error) {
       console.error(error);
     } finally {
