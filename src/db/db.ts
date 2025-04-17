b2a71ed34a69c64675ff918b5e5260e248331fb2
Luka218393 <luka218393@gmail.com>
2025-04-17 18:07:12 +0200

    Update db.ts

--- src/db/db.ts
+++ src/db/db.ts
@@ -10,10 +10,19 @@ const databaseConnectionObject = {
   database: 'studysnap',
 };
 
+const databaseConnectionObject2 = {
+  host: 'mysql-20aad0cb-studysnapdatabse-674b.f.aivencloud.com',
+  port: 15740,
+  user: 'avnadmin',
+  password: process.env.MY_SQL_ONLINE_PASSWORD,
+  database: 'defaultdb',
+};
+
+
 let pool: any;
 export function getPool() {
   if (!pool) {
-    pool = createPool(databaseConnectionObject).promise();
+    pool = createPool(databaseConnectionObject2).promise();
   }
   return pool;
 }
